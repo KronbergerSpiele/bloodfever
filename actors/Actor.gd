34b01ed2d1a600002bac90f8fc4c9347a425798d
Johannes Goslar <johannes@sphere.me>
2020-06-24 02:18:15 +0200

    prepare levels and actors

--- actors/Actor.gd
+++ actors/Actor.gd
@@ -1,17 +1,41 @@
-extends Node2D
+extends RigidBody2D
 
+func _init():
+	inertia = INF
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimatedSprite.play('R000')
-	pass # Replace with function body.
 
+func _process(_delta: float):
+	var vel = linear_velocity.length()
+	if vel < 1:
+		$AnimatedSprite.stop()
+	else:
+		if !$AnimatedSprite.playing:
+			$AnimatedSprite.play()
+		var rotation = linear_velocity.normalized().angle()
+		var nextOrientation = r315OrientationForRotation(rotation)
+		if $AnimatedSprite.animation != nextOrientation:
+			$AnimatedSprite.play(nextOrientation)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func r315OrientationForRotation(rotation: float) -> String:
+	if rotation <= - PI * 7 / 8:
+		return "R180"
+	elif rotation > - PI * 7 / 8 and rotation <= - PI * 5 / 8:
+		return "R135"
+	elif rotation > - PI * 5 / 8 and rotation <= - PI * 3 / 8:
+		return "R090"
+	elif rotation > - PI * 3 / 8 and rotation <= - PI * 1 / 8:
+		return "R045"
+	elif rotation > - PI * 1 / 8 and 	rotation <= PI * 1 / 8:
+		return "R000"
+	elif rotation > PI * 1 / 8 and rotation <= PI * 3 / 8:
+		return "R315"
+	elif rotation > PI * 3 / 8 and rotation <= PI * 5 / 8:
+		return "R270"
+	elif rotation > PI * 5 / 8 and rotation <= PI * 7 / 8:
+		return "R225"
+	elif rotation > PI * 7 / 8:
+		return "R270"
+	else:
+		return "R000"
