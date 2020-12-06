30b105a355f4e4c9d1e5c104ecd1152b79c9fe02
Johannes Goslar <johannes@sphere.me>
2020-12-06 20:45:28 +0100

    more fsharp

--- actors/ActorFs.fs
+++ actors/ActorFs.fs
@@ -1,23 +1,133 @@
 namespace BloodFeverFs
 
-open System
 open Godot
- 
+open System.Collections.Generic
+open System
+
 type ActorFs() =
     inherit RigidBody2D()
- 
-     override x.Equals(yobj) =
-        match yobj with
-            | :? ActorFs as y -> (x.GetInstanceId() = y.GetInstanceId())
-            | _ -> false
 
-    override x.GetHashCode() = System.Convert.ToInt32(x.GetInstanceId())
+    [<Export>]
+    let mutable hitpoints = 100
+
+    [<Export>]
+    let damage = 2
+
+    member this._AnimatedSprite() =
+        let path = new NodePath "AnimatedSprite"
+        let node = this.GetNode path
+        let sprite = node :?> AnimatedSprite
+        sprite
+
+    member this.TouchingBodies = new SortedSet<ActorFs>()
+
+    override this.Equals(yobj) =
+        match yobj with
+        | :? ActorFs as y -> (this.GetInstanceId() = y.GetInstanceId())
+        | _ -> false
+
+    override x.GetHashCode() =
+        System.Convert.ToInt32(x.GetInstanceId())
 
     interface System.IComparable with
-      member x.CompareTo yobj =
-          match yobj with
-              | :? ActorFs as y -> x.GetInstanceId().CompareTo(y.GetInstanceId())
-              | _ -> invalidArg "yobj" "cannot compare values of different types"
+        member x.CompareTo yobj =
+            match yobj with
+            | :? ActorFs as y -> x.GetInstanceId().CompareTo(y.GetInstanceId())
+            | _ -> invalidArg "yobj" "cannot compare values of different types"
 
-    override this._Ready() = 
-        GD.Print("Hello from F#2!")
+    override this._Ready() =
+        base._Ready ()
+        this.Inertia <- System.Single.PositiveInfinity
+        this._AnimatedSprite().Play "R000"
+
+    override this._Process delta =
+        base._Process delta
+        this.R315HandleCollisions()
+        this.R315HandleGraphics()
+
+    abstract R315Prefix: unit -> string
+    default this.R315Prefix() = ""
+
+    abstract R315HonorRotation: unit -> bool
+    default this.R315HonorRotation() = true
+
+    abstract R315HandleCollisions: unit -> unit
+    default this.R315HandleCollisions() = ()
+
+    abstract R315HandleGraphics: unit -> unit
+
+    default this.R315HandleGraphics() =
+        let sprite = this._AnimatedSprite ()
+        let vel = this.LinearVelocity.Length()
+        this.ZIndex <- int this.Position.y
+        if vel < 1.0f then
+            sprite.Stop()
+        else
+            if not (sprite.Playing) then sprite.Play()
+
+            let nextAnimation =
+                if this.R315HonorRotation() then
+                    this.R315Prefix()
+                    + (this.R315OrientationForRotation(this.LinearVelocity.Normalized().Angle()))
+                else
+                    this.R315Prefix()
+
+            if nextAnimation <> sprite.Animation then sprite.Play nextAnimation
+
+
+    abstract R315OrientationForRotation: float32 -> string
+
+    default this.R315OrientationForRotation rot =
+        let rotation = float rot
+        if rotation <= -Math.PI * 7.0 / 8.0 then
+            "R180"
+        else if rotation > -Math.PI
+                * 7.0
+                / 8.0
+                && rotation <= -Math.PI * 5.0 / 8.0 then
+            "R135"
+        else if rotation > -Math.PI
+                * 5.0
+                / 8.0
+                && rotation <= -Math.PI * 3.0 / 8.0 then
+            "R090"
+        else if rotation > -Math.PI
+                * 3.0
+                / 8.0
+                && rotation <= -Math.PI * 1.0 / 8.0 then
+            "R045"
+        else if rotation > -Math.PI
+                * 1.0
+                / 8.0
+                && rotation <= Math.PI * 1.0 / 8.0 then
+            "R000"
+        else if rotation > Math.PI
+                * 1.0
+                / 8.0
+                && rotation <= Math.PI * 3.0 / 8.0 then
+            "R315"
+        else if rotation > Math.PI
+                * 3.0
+                / 8.0
+                && rotation <= Math.PI * 5.0 / 8.0 then
+            "R270"
+        else if rotation > Math.PI
+                * 5.0
+                / 8.0
+                && rotation <= Math.PI * 7.0 / 8.0 then
+            "R225"
+        else if rotation > Math.PI * 7.0 / 8.0 then
+            "R180"
+        else
+            "R000"
+
+
+    member this.onBodyEntered(body: Node) =
+        match body with
+        | :? ActorFs as actor -> this.TouchingBodies.Add actor
+        | _ -> false
+
+    member this.onBodyExcited(body: Node) =
+        match body with
+        | :? ActorFs as actor -> this.TouchingBodies.Remove actor
+        | _ -> false
--- actors/draco/Draco.cs
+++ actors/draco/Draco.cs
@@ -3,7 +3,7 @@ using System;
 
 public class Draco : Actor
 {
-    public override void r315HandleCollisions()
+    public override void R315HandleCollisions()
     {
         // foreach (var actor in touchingBodies)
         // 	GD.Print(actor);
