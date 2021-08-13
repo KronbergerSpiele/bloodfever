969ecd37d74773dc7d8f4e2971e4477449b6ab1f
Johannes Goslar <johannes@sphere.me>
2021-08-13 02:42:00 +0200

    damage subsystem

--- actors/draco/Draco.cs
+++ actors/draco/Draco.cs
@@ -1,10 +1,5 @@
-using Godot;
+using BloodFeverFs;
 
-public class Draco : Actor
+public class Draco : DracoFs
 {
-    public override void R315HandleCollisions()
-    {
-        // foreach (var actor in touchingBodies)
-        // 	GD.Print(actor);
-    }
 }
