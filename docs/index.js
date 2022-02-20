bc19fd1483ed1bd6ee7ca43ac0b62326a1046df3
Johannes Goslar <jogo@kronberger-spiele.de>
2022-02-20 01:39:17 +0100

    add bridge

--- docs/index.js
+++ docs/index.js
@@ -1,1 +1,14 @@
-console.log("Wrapper loaded");
+/// <reference path="./bridge.js" />
+
+function log(...args) {
+  console.log("JSGDBridge:", ...args);
+}
+
+log("Wrapper loaded");
+/**@type {JSGDBridge} */
+const instance = new JSGDBridge("Johnny");
+log("Bridge Instance", instance);
+const iframe = document.getElementById("game");
+log("Game Frame", iframe);
+iframe.contentWindow.jsgdbridge = instance;
+log("Instance injected");
